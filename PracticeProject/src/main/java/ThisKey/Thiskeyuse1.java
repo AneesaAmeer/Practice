031adf12803df09dc5bcde2334fab7827f40d0a1
ABC <ABC@DESKTOP-SBL6J7B>
2024-04-22 18:39:27 +0530

    modified

--- PracticeProject/src/main/java/ThisKey/Thiskeyuse1.java
+++ PracticeProject/src/main/java/ThisKey/Thiskeyuse1.java
@@ -1,22 +1,22 @@
 package ThisKey;
 
 public class Thiskeyuse1 
-	{
+{
 	public void phrase()
 	{
-	System.out.println("Hi");
+	System.out.println("hello1");
 	this.display();
 	}
 	public void display()
 	{
-	System.out.println("Hello");
+	System.out.println("Hello2");
 	}
 	public static void main(String args[])
 	{
 	Thiskeyuse1 obj=new Thiskeyuse1();	
 	obj.phrase();
 	}
-	}
+}
 
 
 
